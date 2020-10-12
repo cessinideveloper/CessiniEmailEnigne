1d38c489a4a75bee14c57811f5ed21c0169bc90e
Shubhro Banerjee <suvro2711@gmail.com>
2020-10-12 14:38:55 +0530

    deploying after build

---
+++ build/precache-manifest.f1ce30f56617460cfe7caa081ec1cff7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bdcfa2f13971d7ea2a9a1d6ce7ccae2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "393ffd81f1a300d5c242",
+    "url": "/static/css/main.f6d0f8ef.chunk.css"
+  },
+  {
+    "revision": "21c6305b9298659f867b",
+    "url": "/static/js/2.49f507f7.chunk.js"
+  },
+  {
+    "revision": "d39df4a9385d93d435839bd506dd69f0",
+    "url": "/static/js/2.49f507f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "393ffd81f1a300d5c242",
+    "url": "/static/js/main.9f0e9b4f.chunk.js"
+  },
+  {
+    "revision": "906c5d749eb414548b6f",
+    "url": "/static/js/runtime-main.ec1b3196.js"
+  }
+]);
